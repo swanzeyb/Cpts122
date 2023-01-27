9c3fddd78b1cbb3996a2eedde15c929d70d465ff
Ben <swanzeyb2001@gmail.com>
2023-01-27 09:35:06 -0800

    Prepare methods for using hash table

--- PA1/PA1/Data.h
+++ PA1/PA1/Data.h
@@ -2,3 +2,24 @@
 
 #include <stdio.h>
 #include "uthash.h"
+
+typedef enum {
+	NONE = 0,
+	ASLEEP = 1,
+	AWAKE = 2,
+	REALLYAWAKE = 3,
+} Sleep;
+
+typedef struct {
+	char patient[10];
+	char minute[9];
+	double calories;
+	double distance;
+	unsigned int floors;
+	unsigned int heartRate;
+	unsigned int steps;
+	Sleep sleepLevel;
+	UT_hash_handle hh;
+} Fitbit;
+
+
--- PA1/PA1/Main.cpp
+++ PA1/PA1/Main.cpp
@@ -4,15 +4,32 @@
 #include "Data.h"
 
 const int CHAR_LIMIT = 100;
+const char* TARGET = "12cx7";
 
 int main(void) {
 
+	// Open the Fitbit data file
 	FILE* contents = fopen("FitbitData.csv", "r");
 
+	// Check if file opened without error
+	if (contents == NULL) {
+		return 1;
+	}
+
+	// Holds the current line
 	char line[CHAR_LIMIT];
 
+	// Instead of an array, I'm using a hash table because
+	// the time complexity of look up is generally O(1)
+	// and this speeds up checking for duplicates.
+	Fitbit *data = NULL;
+
 	do {
+		// Get each line of the CSV file
 		fgets(line, CHAR_LIMIT, contents);
+
+		// Look at each col of the CSV file
+
 		printf("%s", line);
 	} while (!feof(contents));
 
