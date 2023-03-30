2846e7a11644d583c545631f90081ddd6a26b1b5
Ben <swanzeyb2001@gmail.com>
2023-03-30 11:34:09 -0700

    Cleanup the repo

--- PA6/PA6/BSTNode.cpp
+++ PA6/PA6/BSTNode.cpp
@@ -1,3 +1,9 @@
+/**
+ * @file BSTNode.h
+ * @brief The binary search tree node that holds the data for the morse code table
+ * @author Benjamin Swanzey (benjamin.swanzey@wsu.edu)
+ * @date 2023-03-21
+ */
 
 #include "BSTNode.h"
 
--- PA6/PA6/BSTNode.h
+++ PA6/PA6/BSTNode.h
@@ -1,6 +1,6 @@
 /**
  * @file BSTNode.h
- * @brief A queue implementation for the line simulation
+ * @brief The binary search tree node that holds the data for the morse code table
  * @author Benjamin Swanzey (benjamin.swanzey@wsu.edu)
  * @date 2023-03-21
  */
