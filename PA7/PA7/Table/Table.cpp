7ef809e69db2d80635a668edd6f3007bb4695b2c
Ben <swanzeyb2001@gmail.com>
2023-04-14 00:24:19 -0700

    Add Windows fix to PA7

--- PA7/PA7/Table/Table.cpp
+++ PA7/PA7/Table/Table.cpp
@@ -121,8 +121,15 @@ void Table::readCSV(const string& filePath, const char delimiter, const char esc
             while (ss.get(c)) {
                 if (c == escape) {
                     escapedCol = !escapedCol;
-                } else if ((c == delimiter || c == '\r' || c == '\n') && !escapedCol) {
+                } else if ((c == delimiter || c == '\r' || c == '\n' || ss.peek() == EOF) && !escapedCol) {
                     // At the end of the column value
+
+                    // Windows stringstream handles EOF differently than the Mac implementation :(
+                    #ifdef _WIN32
+                    if (ss.peek() == EOF) {
+                        value += c;
+                    }
+                    #endif
 
                     // Setup our columns
                     if (rowIndex == 0) {
